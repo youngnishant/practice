3541132103895e644b78c36df9bf4e195638bb27
Nishant Sharma <youngnishant@outlook.com>
2020-07-11 20:57:23 +0530

    added uri

--- database/connect.js
+++ database/connect.js
@@ -1,9 +1,9 @@
 import mongoose, { connect } from 'mongoose'
-
+const uri=process.env.MONGODB_URI
 export default function (){
   console.log("connect:"+mongoose.connection.readyState)
 if(!mongoose.connection.readyState){
-    mongoose.connect("mongodb+srv://abc:gettingGood@cluster0-lfanp.mongodb.net/test?retryWrites=true&w=majority", {
+    mongoose.connect(uri, {
         useNewUrlParser: true,
         useFindAndModify: false,
         useCreateIndex: true,
