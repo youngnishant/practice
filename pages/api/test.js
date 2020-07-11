7d2db0ea28b31d77cbefb6c5350eead03a25e09b
Nishant Sharma <youngnishant@outlook.com>
2020-07-11 14:50:49 +0530

    finally build it

--- pages/api/test.js
+++ pages/api/test.js
@@ -1,10 +1,38 @@
+import mongoose from 'mongoose'
+mongoose.connect("mongodb+srv://abc:gettingGood@cluster0-lfanp.mongodb.net/test?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  });
 
-import{express}  from 'express';
-const app = express();
-const router = app.Router();
+  let noteSchema=new mongoose.Schema({
+      content:{type:String},
+      by:{type:String}
+  })
+  const Notes=mongoose.models.notes || mongoose.model('notes', noteSchema);
+  
+  module.exports= async(req, res) => {
+    switch(req.method)  {
+        
+        case "GET": 
+        const allNotes=await Notes.find()
+        res.status(200).json(allNotes)
+        break;
 
-router.get('/',(req,res)=>{
-    res.send('hello user');
+        case "POST":
+            const {content,by} =req.body
+            const note=new Notes({
+                content,by
+            })
+            note.save()
+            res.json({"msg":"success"})
 
-})
-export default router
+    }
+
+
+    /*let note=new Notes({
+        content:"hi there , this is test",
+        by:"Nishant"
+    })*/
+    
+  }
