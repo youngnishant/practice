77ba3cb384b7b63f46c4de6f5851bf666eef151e
Nishant Sharma <youngnishant@outlook.com>
2020-07-12 09:58:46 +0530

    finalizing todo app

--- pages/component/appBar.js
+++ pages/component/appBar.js
@@ -3,9 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -28,13 +26,10 @@ export default function ButtonAppBar() {
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
-          </IconButton>
+          
           <Typography variant="h6" className={classes.title}>
-            Take Knowlez
+           Todo
           </Typography>
-          <Button style={{right:'0',position:"fixed"}}color="inherit">Login</Button>
         </Toolbar>
       </AppBar>
     </div>
